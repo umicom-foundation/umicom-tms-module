71e9c4c0deebe133aa7fa92c2b83f1071c1fdfb1
Sammy Hegab <Sammy.Hegab@outlook.com>
2026-08-28 02:27:30 +0100

    feat(tms): align runtime and productisation contracts

--- src/productisation_contribution.c
+++ src/productisation_contribution.c
@@ -18,7 +18,7 @@ static const UmiProductApplicationAdoption ADOPTION = {
     "org.umicom.tms",
     "Umicom TMS",
     "umicom-tms-console",
-    UMI_PRODUCT_FRONTEND_CONSOLE,
+    UMI_PRODUCT_FRONTEND_FLAG_CONSOLE,
     1,
     1,
     1,
