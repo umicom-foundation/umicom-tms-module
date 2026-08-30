f832a2e7d9e221ee3cb6edaad28d5798acc73409
Sammy Hegab <Sammy.Hegab@outlook.com>
2026-08-30 22:43:32 +0100

    feat(tms): enhance productisation contribution

--- include/umicom/tms/productisation_contribution.h
+++ include/umicom/tms/productisation_contribution.h
@@ -16,7 +16,7 @@
 #ifndef UMICOM_TMS_PRODUCTISATION_CONTRIBUTION_H
 #define UMICOM_TMS_PRODUCTISATION_CONTRIBUTION_H
 
-#include "umicom/application/productisation/adoption.h"
+#include "umicom/application/productisation/session.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -26,6 +26,8 @@ const UmiProductApplicationAdoption *
 umi_tms_productisation_contribution(void);
 UmiStatus umi_tms_productisation_snapshot(
     UmiProductApplicationAdoptionSnapshot *out_snapshot);
+UmiStatus umi_tms_product_session_init(
+    UmiProductApplicationSession *out_session);
 
 #ifdef __cplusplus
 }
